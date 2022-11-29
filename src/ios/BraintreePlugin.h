1d3d35ea17510f17b094f2581bef2b37fbbb6d5c
Jean-Christophe Hoelt <hoelt@fovea.cc>
2022-11-29 12:20:13 +0200

    Cleanup comments
    and remove old copyright as no code remains from the old plugin anymore.

--- src/ios/BraintreePlugin.h
+++ src/ios/BraintreePlugin.h
@@ -1,14 +1,12 @@
 //
 //  BraintreePlugin.h
 //
-//  Copyright (c) 2016 Justin Unterreiner. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 
 @interface BraintreePlugin : CDVPlugin
-- (void)launchDropInWithClientToken:(CDVInvokedUrlCommand *)command;
-- (void)setLogger:(CDVInvokedUrlCommand *)command;
+- (void) launchDropInWithClientToken:(CDVInvokedUrlCommand *)command;
+- (void) setLogger:(CDVInvokedUrlCommand *)command;
 - (void) setVerbosity:(CDVInvokedUrlCommand*)command;
 @end
